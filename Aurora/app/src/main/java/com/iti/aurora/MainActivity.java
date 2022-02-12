45573d1fd88185f6f6c5768adecd594cf0cf134e
mahmoudmorsy201 <mahmoudahmedmorsy@gmail.com>
2022-02-13 00:34:22 +0200

    intial commit

---
+++ Aurora/app/src/main/java/com/iti/aurora/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iti.aurora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
