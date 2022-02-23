988d9f6adcb4c2877425b71ac82a008160a2f69e
Walid Zain <walidzeno@gmail.com>
2022-02-23 19:25:29 +0200

    alarm manager dealing with repo

---
+++ Aurora/app/src/main/java/com/iti/aurora/utils/workmanager/NotifierAlarm.java
@@ -0,0 +1,61 @@
+package com.iti.aurora.utils.workmanager;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import com.iti.aurora.R;
+import com.iti.aurora.home.view.MainActivity;
+
+import java.util.Random;
+
+public class NotifierAlarm extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int NOTIFICATION_ID = new Random(System.currentTimeMillis()).nextInt(120);
+
+
+        Uri alarmsound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        //todo here luncher activity name form notification click
+        Intent intent1 = new Intent(context, MainActivity.class);
+        intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.addParentStack(MainActivity.class);
+        taskStackBuilder.addNextIntent(intent1);
+
+        PendingIntent intent2 = taskStackBuilder.getPendingIntent(1,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel("my_channel_01","hello", NotificationManager.IMPORTANCE_HIGH);
+        }
+
+        Notification notification = builder.setContentTitle(intent.getStringExtra("Message"))
+                .setContentText(intent.getStringExtra("RemindDate")).setAutoCancel(true)
+                .setSound(alarmsound).setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(intent2)
+                .setChannelId("my_channel_01")
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(new Random(System.currentTimeMillis()).nextInt(120), notification);
+
+    }
+}
