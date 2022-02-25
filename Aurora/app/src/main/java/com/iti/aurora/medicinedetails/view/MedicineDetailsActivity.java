cd7bcf18aca5f85fafbddc735cb945b416d236ec
Eslam Esmael <EslamEsmael012@Gmail.com>
2022-02-25 15:55:20 +0200

    Adding deleteMedicine function from medicine details activity

--- Aurora/app/src/main/java/com/iti/aurora/medicinedetails/view/MedicineDetailsActivity.java
+++ Aurora/app/src/main/java/com/iti/aurora/medicinedetails/view/MedicineDetailsActivity.java
@@ -1,15 +1,22 @@
 package com.iti.aurora.medicinedetails.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.iti.aurora.R;
+import com.iti.aurora.database.ConcreteLocalSource;
+import com.iti.aurora.model.Repository;
+import com.iti.aurora.model.RepositoryInterface;
 import com.iti.aurora.model.medicine.Medicine;
 import com.iti.aurora.utils.Constants;
+import com.iti.aurora.utils.dialogs.TwoButtonsDialog;
 
 import java.text.MessageFormat;
 
@@ -29,10 +36,15 @@ public class MedicineDetailsActivity extends AppCompatActivity {
 
     Button activeOrSuspendButton;
 
+    ImageView deleteImageView;
+
+    RepositoryInterface repositoryInterface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine_details);
+        repositoryInterface = Repository.getInstance(ConcreteLocalSource.getInstance(this), this);
 
         medicine = (Medicine) getIntent().getSerializableExtra(Constants.MEDICINE_PASSING_FLAG);
 
@@ -45,6 +57,23 @@ public class MedicineDetailsActivity extends AppCompatActivity {
         remindMeWhenTextView = findViewById(R.id.remindMeWhenTextView);
         instructionTextView = findViewById(R.id.instructionTextView);
         reasonOfTakingUserValueTextView = findViewById(R.id.reasonOfTakingUserValueTextView);
+        deleteImageView = findViewById(R.id.deleteImageView);
+
+        deleteImageView.setOnClickListener(view -> TwoButtonsDialog.TwoButtonsDialogBuilder(
+                MedicineDetailsActivity.this,
+                getResources().getString(R.string.deleteMedicine),
+                getResources().getString(R.string.doYouWantToDeleteThisMedicine),
+                getResources().getString(R.string.deleteMedicine),
+                getResources().getString(R.string.cancel),
+                (dialogInterface, i) -> {
+                    repositoryInterface.deleteMedicine(medicine);
+                    dialogInterface.dismiss();
+                    MedicineDetailsActivity.this.finish();
+                }, (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                }
+        ).show());
+
 
         if (medicine != null) {
             medicineNameTextView.setText(medicine.getName());
